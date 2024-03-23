56ea2862231332df1a960a0fefdfc53f105aff8b
Felix <irame94@gmail.com>
2024-03-23 12:45:37 +0100

    Fixed SwitchMode by adding "manuell" as a mode and removing On and Off, which are handled by the SwitchState

--- Enums/SwitchMode.cs
+++ Enums/SwitchMode.cs
@@ -3,26 +3,20 @@
 namespace Fritz.HomeAutomation.Enums
 {
     /// <summary>
-    /// Mode of the 
+    /// Mode of the Switch
     /// </summary>
     public enum SwitchMode
     {
         /// <summary>
-        /// Device is off
-        /// </summary>
-        [XmlEnum(Name = "0")]
-        Off,
-
-        /// <summary>
-        /// Device is on
-        /// </summary>
-        [XmlEnum(Name = "1")]
-        On,
-
-        /// <summary>
         /// Device is in auto mode
         /// </summary>
         [XmlEnum(Name = "auto")]
-        Auto
+        Auto,
+
+        /// <summary>
+        /// Device is in manual mode
+        /// </summary>
+        [XmlEnum(Name = "manuell")]
+        Manual
     }
 }
